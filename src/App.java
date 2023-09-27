2d60bf50ebd23f41c2d21cc56187843ff2c2b731
Mohammad Abdullah <mohammadabdullah103@gmail.com>
2023-09-27 17:52:07 -0400

    first commit

---
+++ src/App.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Scanner;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class App {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        
+
+        public FastReader() {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));   
+        }
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());        //Tokenizes the input here, and stores it in st
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }       //Runs the while intil we have something in st
+            return  st.nextToken();     //Returns the next token
+        }
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        double nextDouble() { 
+            return Double.parseDouble(next());
+        }
+        String nextLine() {
+            String str = "";
+            try {
+                if (st != null && st.hasMoreTokens()) {
+                    str = st.nextToken();
+
+                }
+                else {
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastReader s = new FastReader();    ///Call the input here, and create a new object which contains the input
+
+        String n = s.nextLine();
+        String d = s.nextLine();
+
+        System.out.println("The first line is \n" + n + " and the second line is  \n" + d + ".");
+    }
+}
